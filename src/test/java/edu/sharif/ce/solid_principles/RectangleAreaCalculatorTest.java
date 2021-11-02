8ebb43605665b1b5797c8c928dbdb469cc6bd12e
hbehboudi <hbehboudi@ce.sharif.edu>
2021-11-03 00:45:19 +0330

    TDD Step 1 - Added Rectangle Tests.

---
+++ src/test/java/edu/sharif/ce/solid_principles/RectangleAreaCalculatorTest.java
@@ -0,0 +1,63 @@
+package edu.sharif.ce.solid_principles;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RectangleAreaCalculatorTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void initializeByNegativeWidthTest() {
+        new Rectangle(-1, 2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void initializeByNegativeHeightTest() {
+        new Rectangle(1, -2);
+    }
+
+    @Test
+    public void initialValidRectangleTest() {
+        var rectangle = new Rectangle(1, 2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setWidthByNegativeWidthTest() {
+        var rectangle = new Rectangle(1, 2);
+        rectangle.setWidth(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setHeightByNegativeHeightTest() {
+        var rectangle = new Rectangle(1, 2);
+        rectangle.setHeight(-2);
+    }
+
+    @Test
+    public void setWidthByValidWidthTest() {
+        var rectangle = new Rectangle(1, 2);
+        rectangle.setWidth(3);
+        Assert.assertEquals(3, rectangle.getWidth());
+    }
+
+    @Test
+    public void setHeightByValidHeightTest() {
+        var rectangle = new Rectangle(1, 2);
+        rectangle.setHeight(3);
+        Assert.assertEquals(3, rectangle.getHeight());
+    }
+
+    @Test
+    public void rectangleAreaCalculationTest1() {
+        var width = 2;
+        var height = 2;
+        var rectangle = new Rectangle(width, height);
+        Assert.assertEquals(width * height, rectangle.computeArea());
+    }
+
+    @Test
+    public void rectangleAreaCalculationTest2() {
+        var width = 7;
+        var height = 13;
+        var rectangle = new Rectangle(width, height);
+        Assert.assertEquals(width * height, rectangle.computeArea());
+    }
+}
